a73ed249604785ed6652b3577e8670d67b3765cc
charindu36 <charindumadhusanka801@gmai.com>
2026-09-23 16:05:05 +0530

    db added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,22 @@
 import express from "express";
-// const express = require('express')
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
-const PORT = 5001;
+connectDB();
+
+const PORT=process.env.PORT || 5001;
 
 app.use("/api/notes", notesRoutes);
 
-// app.get("/api/notes", (req,res)=>{
-//     res.status(200).send("you got 5 notes");
-// })
-
-// app.post("/api/notes", (req,res)=>{
-//     res.status(201).json({message:"note created successfully"});
-// })
-
-// app.put("/api/notes/:id", (req,res)=>{
-//     res.status(200).json({message:"note updated successfully"});
-// })
-
-// app.delete("/api/notes/:id", (req,res)=>{
-//     res.status(200).json({message:"note deleted successfully"});
-// })
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
+
+
+//mongodb+srv://charindumadhusanka801_db_user:oR1WdpnXyvM0DyCC@cluster0.wkth6sd.mongodb.net/?appName=Cluster0
